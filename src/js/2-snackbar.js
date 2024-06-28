a2a2f6a20c5a9f73bd5be4e7008c53b527ac6539
Viktor Momotenko <viktor.momotenko@gmail.com>
2024-06-28 21:59:07 +0100

    task-2 done

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,32 @@
+document
+  .getElementById('promiseForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const delay = Number(this.delay.value);
+    const state = this.state.value;
+
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (state === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+
+    promise
+      .then(delay => {
+        iziToast.success({
+          title: 'Success',
+          message: `✅ Fulfilled promise in ${delay}ms`,
+        });
+      })
+      .catch(delay => {
+        iziToast.error({
+          title: 'Error',
+          message: `❌ Rejected promise in ${delay}ms`,
+        });
+      });
+  });
